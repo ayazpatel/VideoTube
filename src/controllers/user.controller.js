d2a270a8f3a844e1dc23cb5726ead8bdf6c46244
ayazpatel <ayazpatel701@gmail.com>
2024-11-13 18:02:31 +0530

    updated commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,8 +4,25 @@ import { ApiResponse } from "../utils/ApiResponse.util.js";
 import { ApiError } from "../utils/ApiError.util.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.util.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating tokens")
+        // throw new ApiError(error.statusCode, error.message)
+    }
+}
 
 const registerUser = asyncHandler( async (req, res) => {
+    
   // get user details from frontend
   // validation - not empty
   // check if user already exists: username, email
@@ -15,13 +32,14 @@ const registerUser = asyncHandler( async (req, res) => {
   // remove password and refresh token field from response
   // check for user creation
   // return res
+    
 
 
   const {fullName, email, username, password } = req.body
   //console.log("email: ", email);
 
   if (
-      [fullName, email, username, password].some((field) => field?.trim() === "")
+    [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
       throw new ApiError(400, "All fields are required")
   }
@@ -100,9 +118,107 @@ const registerUser = asyncHandler( async (req, res) => {
       new ApiResponse(200, createdUser, "User registered Successfully")
   )
 
-} )
+} );
 
-export { registerUser };
+const loginUser = asyncHandler( async (req, res) => {
+    // req body -> data
+    // username or email
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie - secured
+
+    const {email, username, password} = req.body
+
+    // if (!email) {
+    //     throw new ApiError(400, "Email is required")
+    // }
+
+    // if(!username) {
+    //     throw new ApiError(400, "Username is required")
+    // }
+
+    //? !email || !username
+    if (!(email || username)) {
+        throw new ApiError(400, "Email or Username is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{email}, {username}]
+    })
+
+    if (!user) {
+        throw new ApiError(400, "User dose not exist");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+    // make update existing user without making new db call
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+    // user.select("-password -refreshToken")
+
+    const cookieOptions = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, cookieOptions)
+    .cookie("refreshToken", refreshToken, cookieOptions)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken //! bad practice - but good for development practise
+            }, 
+            "User logged in successfully"
+        )
+    );
+
+});
+
+const logoutUser = asyncHandler(
+    async (req, res) => {
+        // const userId = req.user._id
+        await User.findByIdAndUpdate(
+            req.user._id,
+            {
+                $set: {
+                    refreshToken: undefined
+                }
+            },
+            {
+                new: true
+            }
+        )
+
+        const cookieOptions = {
+            httpOnly: true,
+            secure: true
+        }
+
+        return res
+        .status(200)
+        .clearCookie("accessToken", cookieOptions)
+        .clearCookie("refreshToken", cookieOptions)
+        .json(
+            new ApiResponse(200, {}, "User logged Out")
+        )
+    }
+);
+
+
+export { 
+    registerUser,
+    loginUser,
+    logoutUser,
+};
 
 
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { 
+  registerUser,
+  loginUser,
+  logoutUser 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -19,10 +24,21 @@ router.route("/register").post(
   registerUser
 );
 
+router.route("/login").post(loginUser);
+
+//* secured routes
+router.route("/logout").post(
+  verifyJWT,
+  logoutUser
+);
+
+export default router;
+
+
+
+
 // .post((req, res) => {
 //   res.status(200).json({
 //     message: "OK"
 //   })
 // })
-
-export default router;
